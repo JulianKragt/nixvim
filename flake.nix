717d359b3af3f8940d7f0f690fd6f73eea6f8967
Julian Kragt <jkragt@Julians-MacBook-Pro.local>
2025-10-17 09:37:13 +0200

    update

--- flake.nix
+++ flake.nix
@@ -53,5 +53,14 @@
           default = nixvim.lib.${system}.check.mkTestDerivationFromNixvimModule nixvimModule;
         }
       );
+      
+      nixConfig = {
+        extra-substituters = [
+          "https://nix-community.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+      };
     };
 }
