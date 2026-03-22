cb74801535511ad9fa62b72b6afd8538ee2b903e
Julian Kragt <jkragt@julians-macbook-pro.home>
2026-03-22 20:15:44 +0100

    update

--- config/plugins/statusline/lualine.nix
+++ config/plugins/statusline/lualine.nix
@@ -6,7 +6,15 @@
   plugins.lualine = {
     enable = true;
     lazyLoad.enable = false;
-    settings.options.globalstatus = true;
+    settings = {
+      options.globalstatus = true;
+      sections.lualine_c = [
+        {
+          __unkeyed-1 = "filename";
+          path = 1; # relative path
+        }
+      ];
+    };
 
     package = pkgs.vimPlugins.lualine-nvim.overrideAttrs (old: {
       postInstall = ''
